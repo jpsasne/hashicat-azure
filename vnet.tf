c9322b4ebcb2dcd317afcc86f9561e5cb60a8866
Joel <joel@cyberhillpartners.com>
2021-05-13 19:08:29 +0000

    maybe

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/jsasnett-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  #resource_group_name = "testy"
+  resource_group_name = azurerm_resource_group.test.name
 }
